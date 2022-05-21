06eacdbae705c6f7bc1e9b7e947db23d5fc9af3f
Kevin Qu <kevinqu2542@gmail.com>
2022-05-21 12:11:33 -0400

    feat: add intro scene

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const preloader = document.querySelector('#preloader');
 const subPreloaderText = document.querySelector('#subPreloaderText');
 const content = document.querySelector('#content');
-const h1 = document.querySelector('h1');
+const startScreen = document.querySelector('#startScreen');
 
 // game ui selectors
-const start = document.querySelector('#start');
+const startBtn = document.querySelector('#startBtn');
+const gameScreen = document.querySelector('#gameScreen');
+
+// intro selectors
+const intro = document.querySelector('#intro');
+const introText = document.querySelectorAll('.intro');
+const startTimer = document.querySelector('#startTimer');
+
+// question selectors
+const q1 = document.querySelector('#q1');
+const q1Text = document.querySelectorAll('.q1');
 
 // preloader & page animations
 
@@ -15,7 +25,7 @@ window.onload = function () {
     timeline
         .to(preloader, { duration: 2, opacity: 0 })
         .to(content, { duration: 2, opacity: 1 })
-        .from(h1, { duration: 3, y: -50, ease: 'elastic' }, 2);
+        .from(startScreen, { duration: 3, y: -50, ease: 'elastic' }, 2);
     setTimeout(function () {
         preloader.remove();
         subPreloaderText.remove();
@@ -30,6 +40,43 @@ setTimeout(function () {
 
 // trivia game
 
-start.addEventListener('click', function () {
-    console.log('click');
+function setTimer(seconds, el, callback) {
+    let timeLeft = seconds;
+    let timer = setInterval(function () {
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            if (callback === 'questionOne') {
+                questionOne();
+            }
+        } else {
+            el.innerHTML = timeLeft;
+        }
+        timeLeft--;
+    }, 1000);
+}
+
+startBtn.addEventListener('click', function () {
+    const timeline = gsap.timeline();
+    timeline
+        .to(startScreen, { duration: 1, opacity: 0 })
+        .to(gameScreen, { duration: 1, opacity: 1 });
+    setTimeout(function () {
+        startScreen.remove();
+    }, 1000);
+    timeline
+        .to(introText[0], { duration: 1, opacity: 1 }, '<')
+        .to(introText[1], { duration: 2, opacity: 1 })
+        .to(introText[2], { duration: 2, opacity: 1 })
+        .to(introText[3], { duration: 2, opacity: 1 })
+        .to(introText[4], { duration: 2, opacity: 1 });
+    setTimeout(function () {
+        setTimer(3, startTimer, 'questionOne');
+    }, 7000);
 });
+
+function questionOne() {
+    const timeline = gsap.timeline();
+    timeline
+        .to(intro, { duration: 1, opacity: 0 })
+        .to(q1, { duration: 1, opacity: 1 });
+}
